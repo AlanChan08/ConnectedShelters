1251717b3ba4ab57dda817c6bb4172688b2e69d5
AlanChan08 <alan_chan08@hotmail.com>
2015-11-13 18:11:13 +0100

    add google maps

--- app/src/main/java/com/ac/alan/connectedshelters/MainActivity.java
+++ app/src/main/java/com/ac/alan/connectedshelters/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ac.alan.connectedshelters;
 
 import android.app.DownloadManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -58,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
                             //here the adapter will get our record and put it in our listview
                             listAdapter = new com.ac.alan.connectedshelters.ListAdapter(MainActivity.this, shelter.getRecords());
                             listViewShelters.setAdapter(listAdapter);
+                            listViewShelters.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                                    startActivity(intent);
+
+                                }
+
+                                });
                         }
                         catch (IOException e) {
                             e.printStackTrace();
